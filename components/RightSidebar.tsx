4105379c91112261e612012cf8f9342ad45397bd
slicycode <deparisjules180@gmail.com>
2024-05-05 16:52:17 +0200

    feat: add top categories in right sidebar

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,8 +1,12 @@
+import { countTransactionCategories } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import BankCard from "./BankCard";
+import Category from "./Category";
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
+  const categories: CategoryCount[] = countTransactionCategories(transactions);
+
   return (
     <aside className="right-sidebar">
       <section className="flex flex-col pb-8">
@@ -58,6 +62,16 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
             )}
           </div>
         )}
+
+        <div className="mt-10 flex flex-1 flex-col gap-6">
+          <h2 className="header-2">Top categories</h2>
+
+          <div className="space-y-5">
+            {categories.map((category, index) => (
+              <Category key={category.name} category={category} />
+            ))}
+          </div>
+        </div>
       </section>
     </aside>
   );
